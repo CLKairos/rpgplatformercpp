d929fdfbc1480ab486a4ad62fdf7de1d034712e3
CLKairos <christi9nl9rsen@gmail.com>
2025-11-20 16:47:52 +0000

    WOAH TEXT!!

--- src/main.cpp
+++ src/main.cpp
@@ -34,4 +34,23 @@ int main()
     // draw it to the window
     sf::Sprite sprite(texture);
     window.draw(sprite);
+
+    sf::Text text(font); // a font is required to make a text object
+
+    // set the string to display
+    text.setString("Hello world");
+
+    // set the character size
+    text.setCharacterSize(24); // in pixels, not points!
+
+    // set the color
+    text.setFillColor(sf::Color::Red);
+
+    // set the text style
+    text.setStyle(sf::Text::Bold | sf::Text::Underlined);
+
+    ...
+
+    // inside the main loop, between window.clear() and window.display()
+    window.draw(text);
 }
