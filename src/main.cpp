4f97eb54f523c40917c0e837fa2222daf726fad6
CLKairos <christi9nl9rsen@gmail.com>
2025-11-20 16:42:38 +0000

    sprite!!

--- src/main.cpp
+++ src/main.cpp
@@ -16,4 +16,19 @@ int main()
         window.clear(sf::Color::Black);
         window.display();
     }
+
+    // create a 500x500 render-texture
+    sf::RenderTexture renderTexture({500, 500});
+
+    // drawing uses the same functions
+    renderTexture.clear();
+    renderTexture.draw(sprite); // or any other drawable
+    renderTexture.display();
+
+    // get the target texture (where the stuff has been drawn)
+    const sf::Texture& texture = renderTexture.getTexture();
+
+    // draw it to the window
+    sf::Sprite sprite(texture);
+    window.draw(sprite);
 }
