e0f147e770849b6daed3eb30d9abc30b9be82dca
CLKairos <christi9nl9rsen@gmail.com>
2025-11-20 16:45:20 +0000

    sprite = thonk monk

--- src/main.cpp
+++ src/main.cpp
@@ -17,16 +17,19 @@ int main()
         window.display();
     }
 
+    sf::Texture texture;
+    if (!texture.loadFromFile("res/image.png", false, sf::IntRect({10, 10}, {32, 32})))
+    {
+        std::cout << "No image for texture";
+    }
+
     // create a 500x500 render-texture
     sf::RenderTexture renderTexture({500, 500});
 
     // drawing uses the same functions
-    renderTexture.clear();
-    renderTexture.draw(sprite); // or any other drawable
-    renderTexture.display();
-
-    // get the target texture (where the stuff has been drawn)
-    const sf::Texture& texture = renderTexture.getTexture();
+    texture.clear();
+    texture.draw(sprite); // or any other drawable
+    texture.display();
 
     // draw it to the window
     sf::Sprite sprite(texture);
